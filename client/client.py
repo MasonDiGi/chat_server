c8ad04c3f3030a065623f869daaeb9e28ec81c30
Mason DiGiorgio <31572670+MasonDiGi@users.noreply.github.com>
2021-05-26 08:56:50 -0400

    Removed shadowing in client/client.py

--- client/client.py
+++ client/client.py
@@ -14,8 +14,8 @@ client.connect(ADDR)
 erase = '\x1b[1A\x1b[K'
 
 
-def send(msg):
-    message = msg.encode(FORMAT)
+def send(sendMsg):
+    message = sendMsg.encode(FORMAT)
     msg_len = len(message)
     send_len = str(msg_len).encode(FORMAT)
     send_len += b' ' * (HEADER - len(send_len))
@@ -29,8 +29,8 @@ def recvThread():
             msg_len = client.recv(HEADER).decode(FORMAT)
             if msg_len:
                 msg_len = int(msg_len)
-                msg = client.recv(msg_len).decode(FORMAT)
-                print(f"\n{erase}{msg}\n[{uname}]: ", end="")
+                recvMsg = client.recv(msg_len).decode(FORMAT)
+                print(f"\n{erase}{recvMsg}\n[{uname}]: ", end="")
     except Exception as e:
         return e
 
